7a579904edb85e4f5edc7c4e1c32c179d0a175b4
Grant Van Sant <grantwenzlau@gmail.com>
2014-05-30 23:04:57 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Perfume::Application.config.secret_key_base = 'fca098d04442b5c17a510aa767ae61ce41cc4ceafd11dece195a7ab37e0f46cf29c2a9ca176da413be5cb12b11fc925c021af2d67e134cdd7cd6a5470a2ab695'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Perfume::Application.config.session_store :cookie_store, key: '_perfume_session'
