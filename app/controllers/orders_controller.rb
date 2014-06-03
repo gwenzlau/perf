1ac6809209498b4edfc8f1515d3b21be9a2ae1f1
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-02 23:36:26 -0700

    order create fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,21 @@
 class OrdersController < ApplicationController
 	
 	def create
+		@order = Order.create(order_params)
+		if @order.save
+      redirect_to '/', notice: 'Order was successfully posted.'
+    	else
+      render action: 'new'
+		end
+	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+	def order_params
+ 	 params.require(:order).permit(:name, :address, :email, :item)
 	end
 end
