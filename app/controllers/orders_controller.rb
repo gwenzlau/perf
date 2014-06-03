af2f41eeb607162a9ff0e7dcdfa717be94c2a0ab
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-03 08:23:55 -0700

    undo order validate

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,14 @@ class OrdersController < ApplicationController
 	def create
 		@order = Order.create(order_params)
       	if @order.save
-       	 format.html { redirect_to '/', notice: 'Order was successfully sent!' }
-       	 format.json { render json: @order, status: :created, location: @order }
-      	else
-       	 redirect_to '/', notice: 'Please check the fields and try again'
-      	end
+      	 redirect_to '/', notice: 'Order was successfully sent!'
+      	 else
+      	 render action: 'new'
+    	end
 	end
 
 	def new
-		@order = Order.create
-	end
+  	end
 
 	private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,4 @@
 class Order < ActiveRecord::Base
 
 	has_many :items
-	validates :name, presence: true
-	validates :address, presence: true
-	validates :email, presence: true
 end
