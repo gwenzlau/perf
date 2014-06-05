272fc4615490d4a56e9345f45dc3de2c27429404
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-05 10:34:51 -0700

    werk

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,24 @@
 class OrdersController < ApplicationController
 	
+	def index
+	end
+
 	def create
 		@order = Order.create(order_params)
       	if @order.save
-      	 redirect_to '/', notice: 'Order was successfully sent!'
-      	 else
-      	 render action: 'new'
+      		redirect_to @order, :notice => 'Order created!'
+      	else
+      	 	redirect_to root_path, :notice => 'Order not created'
     	end
 	end
 
 	def new
-  	end
+	end
+
+	def add_item_to_order
+		@additem = @item.save
+	end
+
 
 	private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@order = Order.new
+  	@item = Item
   	@items = Item.all
   end
 end
