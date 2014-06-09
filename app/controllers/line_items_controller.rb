11bbcafee73fc6189d69dc96a428b5663acb092c
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-08 18:06:14 -0700

    rm item from cart (lineItem)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,9 +1,17 @@
 class LineItemsController < ApplicationController
+
 	def create
     @item = Item.find(params[:item_id])
     @line_item = LineItem.create!(:cart => current_cart, :item => @item, :quantity => 1)
-    flash[:notice] = "Added #{@item.name} to cart."
+    flash[:notice] = "Added #{@item.name} to sample."
     redirect_to current_cart
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+  	@rm = Line_item.item.destroy
+  	flash[:notice] = "Removed #{@item.name} from sample."
+    redirect_to current_cart
+	end
 
 end
