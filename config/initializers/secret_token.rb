a843b1de67922a2c551bb503dfbbb7aa8b66c2ed
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-14 11:45:40 -0700

    fix secr

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+Perfume::Application.config.secret_token = ENV["RAILS_COOKIE_SECRET"]
 Perfume::Application.config.secret_key_base = ENV["RAILS_COOKIE_SECRET"]
