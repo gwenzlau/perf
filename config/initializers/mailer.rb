406595ed2c747fa7994d33192f32e326c348f08a
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-14 11:19:37 -0700

    mstuff

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,18 +1,11 @@
  require 'tls_smtp'
 
 ActionMailer::Base.smtp_settings =  {
-    :enable_starttls_auto => true,
     :address            => 'smtp.gmail.com',
     :port               => 587,
     :domain             => 'nosyparker.herokuapp.com',
-    :authentication     => :login,
-    :enable_starttls_auto => false,
-    :tls                  => false,
-    :openssl_verify_mode  => 'none',
-    :ssl => false,
     :user_name          => "grantwenzlau",
-    :password           => "gkw7930864"
+    :password           => "gkw7930864",
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   }
-
-  ActionMailer::Base.default_url_options[:host] = "nosyparker.herokuapp.com"
-Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
