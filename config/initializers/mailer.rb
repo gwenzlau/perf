eab67ac7e3a674c2c11ddcf9ac194b73ffa51107
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-14 10:44:08 -0700

    fix mailer config

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,8 +1,4 @@
-config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'nosyparker.herokuapp.com' }
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings =  {
     :enable_starttls_auto => true,
     :address            => 'smtp.gmail.com',
     :port               => 587,
