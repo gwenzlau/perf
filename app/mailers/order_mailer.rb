eaef1af572a059aa79a65ffe7397084f1c6b194d
Grant Van Sant <grantwenzlau@gmail.com>
2014-06-14 10:52:02 -0700

    mail try

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,10 @@
 class OrderMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default bcc: Proc.new { Admin.pluck(:email) },
+  		  from: "from@example.com"
 
   def thanks_email(order)
   	@order = order
   	@url = 'http://localhost:3000/orders'
-  	mail(to: @order.email, subject: 'Thanks for ordering a sample from Nosy Parker!')
+  	mail(to: order.email, subject: 'Thanks for ordering a sample from Nosy Parker!')
   end
 end
