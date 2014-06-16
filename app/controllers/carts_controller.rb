b1c97ec3c0a962064ee42a4f7a4c38d945586295
gio <gio@delisa.com>
2014-06-15 22:28:56 -0400

    remove_item controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,13 @@ class CartsController < ApplicationController
 	def show
     @cart = current_cart
   	end
+
+  def remove_item
+    @cart = current_cart
+    @line_item = cart.line_items.find(params[:id])
+    @line_item.destroy
+    flash[:notice] = "Removed #{@item.name} from sample list."
+    redirect_to current_cart
+  end
+
 end
